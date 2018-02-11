faa7f5b71128856e8b462cb6d92071ef4277a01f
Shea Kusiak <shkusiak@gmail.com>
2018-02-11 16:58:12 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
   # your code here
   new_array = []
-  i = 0
-
+  
   array.each { |item|
-    new_array[i] = item*item
-    i =+ 1
+    squared = item*item
+    new_array.push(squared)
   }
   return new_array
 end
